4884029f8eab998aaa18ce4c299f1e7b132d567a
m-ahmad-amin <hafizmuhammadahmadamin@gmail.com>
2025-12-11 18:19:38 +0500

    Project Configurations

---
+++ HRMSLib/BusinessLogic/CallResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMSLib.BusinessLogic
+{
+    [Serializable]
+    public class CallResult
+    {
+        public bool Successful = true;
+        public string Code = string.Empty;
+        public string Message = string.Empty;
+        public object Value = null;
+    }
+}
---
+++ HRMSLib/DataLayer/EmployeeMaster.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMSLib.DataLayer
+{
+    [Serializable]
+    class EmployeeMaster
+    {
+        public static DataSet View_DeptList(string Search)
+        {
+            try
+            {
+                Database db = new DatabaseProviderFactory().Create("defaultDB");
+                DbCommand cmd = db.GetStoredProcCommand("pr_ProcedureName");
+                db.AddInParameter(cmd, "Search", DbType.String, Search);
+                return db.ExecuteDataSet(cmd);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
